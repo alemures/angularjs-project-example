24da27bd2bfe3f161752222fc9c594a43cc18209
Alejandro Santiago <alemures@hotmail.com>
2018-11-15 17:57:39 +0100

    Minifying css using cssnano and post-css. Updated deps.

--- webpack.prod.js
+++ webpack.prod.js
@@ -3,12 +3,12 @@ const common = require('./webpack.common.js');
 
 const htmlRule = common.module.rules.find(rule => String(rule.test) === '/\\.html$/');
 const htmlLoader = htmlRule.use.find(use => use.loader === 'html-loader');
-const cssRule = common.module.rules.find(rule => String(rule.test) === '/\\.css$/');
-const cssLoader = cssRule.use.find(use => use.loader === 'css-loader');
 htmlLoader.options.minimize = true;
-cssLoader.options.minimize = true;
 
 module.exports = merge(common, {
   mode: 'production',
   devtool: 'source-map',
 });
+
+// Used by postcss setting the 'env' parameter
+process.env.NODE_ENV = 'production';
